d06099409f112139bcdabf9b11a3ef048c58b501
Terry Theis <tltheis@prodigy.net>
2021-07-11 10:09:30 -0500

    init folio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import About from "./Pages/About/About.js";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Footer from "./components/Footer";
+import Resume from "./Pages/Resume/Resume"
+import Contact from "./Pages/Contact/Contact.js"
 
+// we need to render each one of these conditionally on click
 function App() {
+  const [currentPage, handlePageChange] = useState("About");
+  //remember [ state holder, set state function]
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Resume":
+        return <Resume />;
+      case "Contact":
+        return <Contact />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div>
+        {renderPage(currentPage)}
+      </div>
+      <Footer />
     </div>
   );
 }
